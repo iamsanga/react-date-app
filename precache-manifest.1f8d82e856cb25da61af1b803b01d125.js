e9881fd10dbc43734f7a5efa3ae56a30152cba03
iamsanga <iamsanga7@gmail.com>
2020-04-10 18:47:48 +0530

    Updates

---
+++ precache-manifest.1f8d82e856cb25da61af1b803b01d125.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a049f6ebc76f73f5e90b",
+    "url": "/react-date-app/static/css/main.f856133d.chunk.css"
+  },
+  {
+    "revision": "a049f6ebc76f73f5e90b",
+    "url": "/react-date-app/static/js/main.ebf803c4.chunk.js"
+  },
+  {
+    "revision": "228adb9bed4a5bf4e56c",
+    "url": "/react-date-app/static/js/runtime~main.474c1e93.js"
+  },
+  {
+    "revision": "d336b635e6e0b4108315",
+    "url": "/react-date-app/static/js/2.54c58c33.chunk.js"
+  },
+  {
+    "revision": "77e8edcc39858d6f37460dfe4179776a",
+    "url": "/react-date-app/index.html"
+  }
+];
